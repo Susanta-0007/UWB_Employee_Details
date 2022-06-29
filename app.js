42b2645723075bec6fd279a93be763134daa616f
Susanta Gorai <89796600+SusantaGorai08@users.noreply.github.com>
2022-06-29 11:45:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+/* Use javascript and Jquary  */
+
+/* Input */
+$("#input-1").on("click", () => {
+  $("#input-1").css("borderColor", " #6c2cf5");
+});
+$("#input-2").on("click", () => {
+  $("#input-2").css("borderColor", " #6c2cf5");
+});
+$("#input-3").on("click", () => {
+  $("#input-3").css("borderColor", " #6c2cf5");
+});
+$("#input-4").on("click", () => {
+  $("#input-4").css("borderColor", " #6c2cf5");
+});
+$("#input-5").on("click", () => {
+  $("#input-5").css("borderColor", " #6c2cf5");
+});
+$("#input-6").on("click", () => {
+  $("#input-6").css("borderColor", " #6c2cf5");
+});
+/*Submit Button */
+document.getElementById("btn-1").onclick = function () {
+  swal("Thank You 💜", "Form Submitted!", "success");
+};
